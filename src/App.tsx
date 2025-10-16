c2b4ae5b7d05baeeaffa4c6c21b4f1cfd60460b9
Ifeoluwah <spencerife.21@gmail.com>
2025-10-17 00:32:02 +0100

    chore: Initial commit
    - Setup configurations

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+	return (
+		<main className="one font-spartan text-2xl font-bold">
+			<section className="flex items-center justify-center w-screen min-h-[100dvh] one:bg-theme-1-navy-850">
+				<div className="w-2xl">
+					<header className="flex justify-between text-theme-1-white">
+						<h1>Calc</h1>
+						<p>theme</p>
+					</header>
+				</div>
+			</section>
+		</main>
+	);
+}
+
+export default App;
